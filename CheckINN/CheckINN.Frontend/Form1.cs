ce5099c3ea264f58028c1f9718d419068ab30c44
Lukas Praninskas <l.praninskas@gmail.com>
2017-09-24 23:22:27 +0300

    Merge new files, rename sample folder, attempt a read

--- CheckINN/CheckINN.Frontend/Form1.cs
+++ CheckINN/CheckINN.Frontend/Form1.cs
@@ -1,27 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using tessnet2;
 
 namespace CheckINN.Frontend
 {
     public partial class Form1 : Form
     {
+        private tessnet2.Tesseract tess = null;
+
         public Form1()
         {
             InitializeComponent();
             InitTesseract();
+
+            if (tess == null)
+            {
+                throw new Exception("Failed to load tessaract");
+            }
+
+            foreach (var word in DoOCR(new Bitmap(@"samples/cekis_lidl.bmp")))
+            {
+                Console.WriteLine(word.ToString());
+            }
         }
 
         public void InitTesseract()
         {
-            var tess = new tessnet2.Tesseract();
+
+            tess = new tessnet2.Tesseract();
             tess.Init(@"tessdata", "lt", true);
+        }
+
+        public List<Word> DoOCR(Bitmap image)
+        {
+            return tess.DoOCR(image, Rectangle.Empty);
         }
     }
 }
