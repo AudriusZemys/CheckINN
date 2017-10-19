9d0c354768ada1cf2dbb96e845b62c4ebbc6d59a
Lukas Praninskas <l.praninskas@gmail.com>
2017-10-19 21:37:54 +0300

    Move check processor

--- CheckINN.Domain/Processing/ICheckProcessor.cs
+++ CheckINN.Domain/Processing/ICheckProcessor.cs
@@ -6,21 +6,4 @@ namespace CheckINN.Domain.Processing
     public interface ICheckProcessor : IProcessor<Check>
     {
     }
-
-    public class BasicCheckProcessor : ICheckProcessor {
-        public bool TryProcess(Check item)
-        {
-            bool result = true;
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-                result = false;
-                Console.Write(ex.Message);
-            }
-            return result;
-        }
-    }
 }
