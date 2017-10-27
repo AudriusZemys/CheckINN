49309f9e0c90f3b3cd1c4136953c1899f93c0b92
Lukas Praninskas <l.praninskas@adform.com>
2017-10-27 12:26:52 +0300

    Working basic pipeline

--- CheckINN.Domain/Parser/IShopParser.cs
+++ CheckINN.Domain/Parser/IShopParser.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
 using CheckINN.Domain.Entities;
 
 namespace CheckINN.Domain.Parser
@@ -8,23 +6,5 @@ namespace CheckINN.Domain.Parser
     public interface IShopParser
     {
         IEnumerable<Product> ParseProductList(string text);
-    }
-
-    public class SimpleShopParser : IShopParser
-    {
-        public IEnumerable<Product> ParseProductList(string text)
-        {
-            var regex = new Regex(@"(.+)\n");
-            var match = regex.Match(text);
-            if (!match.Success)
-            {
-                throw new Exception("Not matched");
-            }
-
-            foreach (Group matchGroup in match.Groups)
-            {
-                yield return new Product(matchGroup.Value, 0);
-            }
-        }
     }
 }
---
+++ CheckINN.Domain/Parser/SimpleShopParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using CheckINN.Domain.Entities;
+
+namespace CheckINN.Domain.Parser
+{
+    public class SimpleShopParser : IShopParser
+    {
+        public IEnumerable<Product> ParseProductList(string text)
+        {
+            var regex = new Regex(@"(.+)\n", RegexOptions.Multiline);
+            var matches = regex.Matches(text);
+            if (matches.Count < 1)
+            {
+                throw new Exception("Not matched");
+            }
+
+            foreach (Group matchGroup in matches)
+            {
+                yield return new Product(matchGroup.Value.TrimEnd('\n'), 0);
+            }
+        }
+    }
+}
