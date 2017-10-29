23cb2cc0e5c9564bdb2d980d51a2810f6d48248e
Lukas Praninskas <l.praninskas@adform.com>
2017-10-29 22:16:30 +0200

    Add a test to check double iteration of enumerable

---
+++ CheckInn.Domain.Tests/Entities/CheckBodyTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CheckINN.Domain.Entities;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace CheckInn.Domain.Tests.Entities
+{
+    [TestFixture]
+    class CheckBodyTest
+    {
+        [Test]
+        public void CheckForSingleEnumerableIteration()
+        {
+            var fixture = new Fixture().Customize(new MultipleCustomization());
+            var check = fixture.Create<CheckBody>();
+
+            var failed = false;
+            try
+            {
+                var first = check[1];
+                var products = check.Products;
+            }
+            catch (Exception)
+            {
+                failed = true;
+            }
+
+            Assert.False(failed);
+        }
+    }
+}
