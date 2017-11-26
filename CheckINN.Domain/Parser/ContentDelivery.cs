d74004b6bf5f7bd2fa80c73d3e8252a2cafdb0a5
Povilas Stankevicius <pov.stankevicius@gmail.com>
2017-11-26 17:23:57 +0200

    Substitute relative resource paths for absolute ones

--- CheckINN.Domain/Parser/ContentDelivery.cs
+++ CheckINN.Domain/Parser/ContentDelivery.cs
@@ -7,7 +7,8 @@ namespace CheckINN.Domain.Parser
     {
         public static IEnumerable<string> getContent()
         {
-            return File.ReadLines(@"C:\Users\a\source\repos\CheckINN\CheckINN.Frontend\Parsing.Test.Data\checkTest.txt");
+            //return File.ReadLines(@"C:\Users\a\source\repos\CheckINN\CheckINN.Frontend\Parsing.Test.Data\checkTest.txt");
+            return File.ReadLines(@"..\..\ParserSampleData\checkTest.txt");
         }
     }
 }
