8c28522a78e07a9ebf87c8d0ca7b4b7b881156be
Povilas Stankevicius <pov.stankevicius@gmail.com>
2017-11-26 17:25:13 +0200

    Comply with naming convection

--- CheckINN.Domain/Parser/ContentDelivery.cs
+++ CheckINN.Domain/Parser/ContentDelivery.cs
@@ -5,7 +5,7 @@ namespace CheckINN.Domain.Parser
 {
     class ContentDelivery
     {
-        public static IEnumerable<string> getContent()
+        public static IEnumerable<string> GetContent()
         {
             //return File.ReadLines(@"C:\Users\a\source\repos\CheckINN\CheckINN.Frontend\Parsing.Test.Data\checkTest.txt");
             return File.ReadLines(@"..\..\ParserSampleData\checkTest.txt");
