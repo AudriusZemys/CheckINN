c7854e6acc666f76ebeaa62ddc740ea3e65eec69
Povilas Stankevicius <pov.stankevicius@gmail.com>
2017-11-27 01:26:51 +0200

    Move test data and associated classes to project

--- CheckInn.Domain.Tests/Parser/ContentDelivery.cs
+++ CheckInn.Domain.Tests/Parser/ContentDelivery.cs
@@ -7,8 +7,7 @@ namespace CheckINN.Domain.Parser
     {
         public static IEnumerable<string> GetContent()
         {
-            //return File.ReadLines(@"C:\Users\a\source\repos\CheckINN\CheckINN.Frontend\Parsing.Test.Data\checkTest.txt");
-            return File.ReadLines(@"..\..\ParserSampleData\checkTest.txt");
+            return File.ReadLines(@".\ParserSampleData\checkTest.txt");
         }
     }
 }
