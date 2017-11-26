e8ba0ee469622c26530315ee3334440f70632b12
Audrius <a.zemys@gmail.com>
2017-11-26 23:59:39 +0200

    Activity updates

--- Front/app/src/main/java/com/checkinn/front/LoginActivity.java
+++ Front/app/src/main/java/com/checkinn/front/LoginActivity.java
@@ -3,6 +3,7 @@ package com.checkinn.front;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -92,6 +93,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+
+    public void toListActivity(View view){
+        Intent intent = new Intent(LoginActivity.this, ListActivity.class);
+        startActivity(intent);
     }
 
     private void populateAutoComplete() {
