93f3f41e649d73eeff100618630e59c9eb30c4ff
Povilas Stankevicius <pov.stankevicius@gmail.com>
2017-11-27 02:51:18 +0200

    Add unit test

--- CheckInn.Domain.Tests/Parser/ContentDelivery.cs
+++ CheckInn.Domain.Tests/Parser/ContentDelivery.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace CheckINN.Domain.Tests.Parser
 {
@@ -7,7 +10,12 @@ namespace CheckINN.Domain.Tests.Parser
     {
         public static IEnumerable<string> GetContent()
         {
-            return File.ReadLines(@".\ParserSampleData\checkTest.txt");
+            var sample = Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceStream("CheckInn.Domain.Tests.Parser.ParserSampleData.checkTest.txt");
+            StreamReader reader = new StreamReader(sample);
+            string text = reader.ReadToEnd();
+            return text.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
         }
     }
 }
---
+++ CheckInn.Domain.Tests/Parser/ParserTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using CheckINN.Domain.Parser;
+using CheckINN.Domain.Tests.Parser;
+using NUnit.Framework;
+using static NUnit.Framework.CollectionAssert;
+
+namespace CheckInn.Domain.Tests.Parser
+{
+    [TestFixture]
+    public class ParserTest
+    {
+        private List<Tuple<string, double>> _expectedResult;
+
+        [SetUp]
+        public void Setup()
+        {
+            _expectedResult = new List<Tuple<string, double>>
+            {
+                new Tuple<string, double>("Citrinos (5 d.)", 1.39),
+                new Tuple<string, double>("Malta kava JACOBS KRONUNG", 4.99),
+                new Tuple<string, double>("Kepintos ir sūdytos pistacijos", 14.74),
+                new Tuple<string, double>("Brendis J. P. CHENET RESERVE IMPERIALE, 38%", 16.99),
+                new Tuple<string, double>("Kepintos saulėgrąžos žM", 1.48),
+                new Tuple<string, double>("Tortas JUODOJI ROŽĖ", 6.97)
+            };
+        }
+
+        [Test]
+        public void ParseProductList_OutputsAListOfProducts()
+        {
+            // arrange
+            CheckINN.Domain.Parser.Parser parser = new CheckINN.Domain.Parser.Parser(ContentDelivery.GetContent());
+
+            // act
+            parser.Parse();
+
+            // assert
+            AreEquivalent(_expectedResult, parser.Products);
+        }
+    }
+}
