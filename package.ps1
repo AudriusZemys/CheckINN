f49247f59dde38eed79c9f1c24072d384fcdb12b
Lukas Praninskas <l.praninskas@gmail.com>
2017-12-06 00:22:42 +0200

    Update packaging

---
+++ package.ps1
@@ -0,0 +1,14 @@
+param (
+    [string] $project = $(throw "Must provide a project name `"-project blablabla`"")
+    [string] $version = $(throw "Must provide a version string `"-version 0.0.0.0`"")
+)
+
+function Zip-Files( $zipfilename, $sourcedir )
+{
+   Add-Type -Assembly System.IO.Compression.FileSystem
+   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+   [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir,
+        $zipfilename, $compressionLevel, $false)
+}
+
+Zip-Files "C:\projects\checkinn\$project.$version.zip" "C:\projects\checkinn\$project\bin\Release\"
