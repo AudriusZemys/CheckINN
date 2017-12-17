c84a3cd5f1c97f86d744af8c8c7bfdcd99d6ddb0
Povilas Stankevicius <pov.stankevicius@gmail.com>
2017-12-17 16:43:51 +0200

    Add additional annotations

--- Front/app/src/main/java/com/checkinn/front/database/entities/Item.java
+++ Front/app/src/main/java/com/checkinn/front/database/entities/Item.java
@@ -1,16 +1,23 @@
 package com.checkinn.front.database.entities;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "item")
 class Item {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    @ColumnInfo(name = "item_name")
     public String itemName;
+    @ColumnInfo(name = "shop_name")
     public String shopName;
+    @ColumnInfo(name = "price")
     public double price;
 
-    public Item(String itemName, String shopName, double price) {
-        this.itemName = itemName;
-        this.shopName = shopName;
-        this.price = price;
-    }
+//    public Item(String itemName, String shopName, double price) {
+//        this.itemName = itemName;
+//        this.shopName = shopName;
+//        this.price = price;
+//    }
 }
