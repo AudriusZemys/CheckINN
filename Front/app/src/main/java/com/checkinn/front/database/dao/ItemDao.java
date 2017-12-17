14a1ea678aa0e6b3d61c82a97dcbaef25069106f
Povilas Stankevicius <pov.stankevicius@gmail.com>
2017-12-17 17:01:08 +0200

    Add dao

---
+++ Front/app/src/main/java/com/checkinn/front/database/dao/ItemDao.java
@@ -0,0 +1,25 @@
+package com.checkinn.front.database.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.checkinn.front.database.entities.Item;
+
+@Dao
+public interface ItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertItems(Item... items);
+
+    @Update
+    public void updateItems(Item... items);
+
+    @Delete
+    public void deleteItems(Item... items);
+
+    @Query("SELECT * FROM item")
+    public Item[] loadAllItems();
+}
--- Front/app/src/main/java/com/checkinn/front/database/entities/Item.java
+++ Front/app/src/main/java/com/checkinn/front/database/entities/Item.java
@@ -5,7 +5,7 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
 @Entity(tableName = "item")
-class Item {
+public class Item {
     @PrimaryKey(autoGenerate = true)
     public int id;
     @ColumnInfo(name = "item_name")
